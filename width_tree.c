ff71e0d254a1863a37265d387f2268069f99020b
Felipe Prevelato <fprevela@e1z3r8p2.42.us.org>
2019-10-05 12:51:11 -0700

    code sample + subject

---
+++ width_tree.c
@@ -0,0 +1,35 @@
+struct				s_node
+{
+	int				value;
+	struct s_node	*left;
+	struct s_node	*right;
+};
+
+int					m(int i, int j)
+{
+	return (i > j ? i : j);
+}
+
+int					count(struct s_node *n, int *max)
+{
+	if (!n)
+		return (0);
+	int				right = count(n->right, max);
+	int				left = count(n->left, max);
+	int				local = m(right, left) + 1;
+	int				w = left + right + 1;
+	if (*max < w)
+		*max = w;
+	return (local);
+}
+
+int					width_tree(struct s_node *n)
+{
+	int				max = 1;
+	int				local = 0;
+
+	if (!n)
+		return (0);
+	local = count(n, &max);
+	return (max);
+}
